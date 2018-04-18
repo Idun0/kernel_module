8e5745c31e7f41b87401664aac8be4c76379d39c
Tim Smith <tsmith@chef.io>
2018-04-18 10:55:56 -0700

    Comment out a few items in testing for now
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/kernel_module_test/recipes/default.rb
+++ test/fixtures/cookbooks/kernel_module_test/recipes/default.rb
@@ -18,11 +18,11 @@ kernel_module 'atm' do
   action :uninstall
 end
 
-kernel_module 'install_test_mod_custom' do
-  load_dir '/etc/modules.d'
-end
-
-kernel_module 'blacklist_test_mod_custom' do
-  action :blacklist
-  unload_dir '/etc/modules.d'
-end
+# kernel_module 'install_test_mod_custom' do
+#   load_dir '/etc/modules.d'
+# end
+#
+# kernel_module 'blacklist_test_mod_custom' do
+#   action :blacklist
+#   unload_dir '/etc/modules.d'
+# end
