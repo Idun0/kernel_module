4a1822ba20c5e7189f16d39b5b596d5c97376ca4
Tim Smith <tsmith@chef.io>
2018-04-18 11:19:20 -0700

    Release 1.1.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'MIT'
 description       'Load kernel modules'
-version           '1.0.1'
+version           '1.1.0'
 
 supports          'ubuntu'
 supports          'debian'
